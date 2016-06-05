9526ddf91df1e8dcbea58e222e121dcab3152ef6
Guyren Howe <guyren@relevantlogic.com>
2016-06-04 23:56:11 -0700

    Convert params to symbol keys so it can be used in a function call

--- lib/frest/http_transport.rb
+++ lib/frest/http_transport.rb
@@ -5,8 +5,9 @@ module Frest
     def new_transport source:
       Proc.new do |env|
         req = Rack::Request.new(env)
+        params = Hash[req.params.map{|k, v| [k.to_sym, v]}]
 
-        initial_response = source.call
+        initial_response = source.call **params
         response = Frest::Convert.convert from: initial_response, to: 'html'
 
         [200, {'Content-Type' => 'text/html'}, [response]]
